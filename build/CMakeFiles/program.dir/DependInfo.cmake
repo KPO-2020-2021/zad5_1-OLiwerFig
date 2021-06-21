7e80285c32453a02befff6a71265f73805fa9056
OLiwerFig <259754@student.pwr.edu.pl>
2021-06-22 01:16:09 +0200

    przepraszam za opóznienie

---
+++ build/CMakeFiles/program.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/oliwer/Pobrane/Oliwer1/src/Bryla.cpp" "/home/oliwer/Pobrane/Oliwer1/build/CMakeFiles/program.dir/src/Bryla.cpp.o"
+  "/home/oliwer/Pobrane/Oliwer1/src/Dron.cpp" "/home/oliwer/Pobrane/Oliwer1/build/CMakeFiles/program.dir/src/Dron.cpp.o"
+  "/home/oliwer/Pobrane/Oliwer1/src/Graniastoslup6.cpp" "/home/oliwer/Pobrane/Oliwer1/build/CMakeFiles/program.dir/src/Graniastoslup6.cpp.o"
+  "/home/oliwer/Pobrane/Oliwer1/src/Macierz3x3.cpp" "/home/oliwer/Pobrane/Oliwer1/build/CMakeFiles/program.dir/src/Macierz3x3.cpp.o"
+  "/home/oliwer/Pobrane/Oliwer1/src/Plaszczyzna.cpp" "/home/oliwer/Pobrane/Oliwer1/build/CMakeFiles/program.dir/src/Plaszczyzna.cpp.o"
+  "/home/oliwer/Pobrane/Oliwer1/src/Prostopadloscian.cpp" "/home/oliwer/Pobrane/Oliwer1/build/CMakeFiles/program.dir/src/Prostopadloscian.cpp.o"
+  "/home/oliwer/Pobrane/Oliwer1/src/Scena.cpp" "/home/oliwer/Pobrane/Oliwer1/build/CMakeFiles/program.dir/src/Scena.cpp.o"
+  "/home/oliwer/Pobrane/Oliwer1/src/example.cpp" "/home/oliwer/Pobrane/Oliwer1/build/CMakeFiles/program.dir/src/example.cpp.o"
+  "/home/oliwer/Pobrane/Oliwer1/src/lacze_do_gnuplota.cpp" "/home/oliwer/Pobrane/Oliwer1/build/CMakeFiles/program.dir/src/lacze_do_gnuplota.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ENABLE_DOCTEST_IN_LIBRARY"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../include"
+  "../external/doctest/doctest"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/oliwer/Pobrane/Oliwer1/tests
+# Build directory: /home/oliwer/Pobrane/Oliwer1/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(program.unit_tests "/home/oliwer/Pobrane/Oliwer1/build/unit_tests")
+set_tests_properties(program.unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/oliwer/Pobrane/Oliwer1/tests/CMakeLists.txt;26;add_test;/home/oliwer/Pobrane/Oliwer1/tests/CMakeLists.txt;0;")
